ee27c6562a25e3771bf25a8e57b9b83a0647f5c1
yashG <yrg.creation.ai@gmail.com>
2025-12-29 22:07:07 +0530

    top menu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,21 +5,16 @@ const Sidebar = () => {
     <div className='sidebar-holder'>
         <div className="top-row">
            <div className="logo">
-            <div className="logo-img">
+               <div className="logo-img">
                  <img src="./src/assets/YV logo2.PNG" alt=""  className='log-img' />
-            </div>
-              <h2>YVerse</h2>
+               </div>
+               <h2>YVerse</h2>
            </div>
-            <div className="menu-icon">
-              <div className="menu-ham">
-                  <div className="menu-bar"></div>
-                  <div className="menu-bar"></div>
-                  <div className="menu-bar"></div>
-              </div>
-              {/* <div className="menu">
-                mobile menu will be here
-              </div> */}
-            </div>
+           <div className="menu-ham">
+               <div className="menu-bar"></div>
+               <div className="menu-bar"></div>
+               <div className="menu-bar"></div>
+           </div>
         </div>
     </div>
   )
